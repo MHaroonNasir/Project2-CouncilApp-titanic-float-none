3f9820a1d8cdcbed108656b322bb1dbd6f9bde25
brayson98 <brayson@live.com>
2023-03-28 16:33:37 +0100

    added my posts to user profile page

--- client/assets/js/userProfile.js
+++ client/assets/js/userProfile.js
@@ -19,17 +19,42 @@ async function getUserInfo() {
           },
         });
         const userDetails = await userDetailsResponse.json();
-    const { username, email, posts } = userDetails;
+    const { username, email } = userDetails;
     document.getElementById("username").innerText = username;
     document.getElementById("email").innerText = email;
-    const postsList = document.getElementById("posts");
-    posts.forEach((post) => {
-      const listItem = document.createElement("li");
-      postLink.innerText = post.title;
-      postsList.appendChild(listItem);
+   
+    const userPostsResponse = await fetch(`http://localhost:3000/posts/user/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
     });
+    const userPosts = await userPostsResponse.json();
+    const postsList  = document.getElementById("user-posts");
+    if (userPosts.length > 1){
+        userPosts.forEach((post) => {
+            const card = document.createElement("div");
+            card.classList.add("card");
+            const cardTitle = document.createElement("h3");
+            cardTitle.innerText = post.title;
+            const cardContent = document.createElement("p");
+            cardContent.innerText = post.content;
+            const cardCategory = document.createElement("p");
+            cardCategory.innerText = `Category: ${post.category}`;
+            card.appendChild(cardTitle);
+            card.appendChild(cardContent);
+            card.appendChild(cardCategory);
+            postsList.appendChild(card);
+        });
+    } else {
+        const listItem = document.createElement("li");
+        listItem.innerText = "No posts available";
+        postsList.appendChild(listItem);
+      }
   } catch (error) {
     
   }
 }
+
 getUserInfo();
