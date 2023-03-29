ee66966641c9bfb9aa82ba22f838a455725d4b9f
Silvia Neves <siltatiana.sn@gmail.com>
2023-03-29 12:10:51 +0100

    buttons func

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -20,64 +20,91 @@ async function getUserId() {
     .then((response) => response.json())
     .then((data) => {
       //console.log(data)
-      dataHolder = data
-    })
-    return dataHolder;
+      dataHolder = data;
+    });
+  return dataHolder;
 }
 
-async function volunteer (e) {
-  console.log("called", e.target.classList[1])
+async function volunteer(e) {
+  console.log("called", e.target.classList[1]);
   const cardId = e.target.classList[1];
 
   const data = await getUserId();
   console.log(data["user_id"]);
 
   await fetch(`http://127.0.0.1:3000/volunteer/`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       post_id: parseInt(e.target.classList[1]),
-      user_id: parseInt(data["user_id"])
+      user_id: parseInt(data["user_id"]),
+    }),
+  })
+    .then((response) => {
+      if (!response) {
+        throw new Error("No valid network response!");
+      }
+      return response.json();
     })
-  })
-  .then(response => {
-    if (!response) {
-      throw new Error("No valid network response!");
-    }
-    return response.json();
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    .catch((error) => {
+      console.error("Error:", error);
+    });
   console.log("done");
-};
+}
 
 const applyToVolunteer = () => {
   const applyToPost = Array.from(document.getElementsByClassName("btn-apply"));
   //console.log(applyToPost)
-  applyToPost.forEach(button => {
-    button.addEventListener("click", volunteer)
+  applyToPost.forEach((button) => {
+    button.addEventListener("click", () => isLogin);
   });
 };
+
+const logoutBtn = document.getElementById("logout");
+const createPostBtn = document.getElementById("createPost");
+
+const isLogin = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    console.log("Im in");
+    return true;
+  } else {
+    console.log("Im not in");
+    return false;
+  }
+};
+
+const logIn = isLogin();
+
+if (!logIn) {
+  logoutBtn.innerText = "Log In";
+}
+
+if (!logIn) {
+  createPostBtn.addEventListener("click", (e) => {
+    console.log(e);
+    window.location.href = "login.html";
+  });
+} else {
+  createPostBtn.addEventListener("click", (e) => {
+    console.log(e);
+    window.location.href = "createPost.html";
+  });
+}
 
 async function getAllPosts() {
   await fetch(`http://localhost:3000/posts`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
       posts = data.map((post) => {
         const card = postCardTemplate.content.cloneNode(true).children[0];
         const title = card.querySelector("[data-title]");
         const content = card.querySelector("[data-content]");
         const category = card.querySelector("[data-category]");
-
-        //const buttonClass = card.querySelector("[button-class]")
-        const buttonClass = card.querySelector("[btn-apply]")
-        console.log("post", buttonClass, post.post_id)
+        const buttonClass = card.querySelector("[btn-apply]");
         buttonClass.classList.add(post.post_id);
-
         title.textContent = post.title;
         content.textContent = post.content;
         category.textContent = `Category: ${post.category}`;
@@ -90,31 +117,7 @@ async function getAllPosts() {
         };
       });
     });
-  
   applyToVolunteer();
 }
+
 getAllPosts();
-
-const logoutBtn = document.getElementById("logout");
-const createPostBtn = document.getElementById("createPost");
-
-const isLogin = () => {
-  const token = localStorage.getItem("token");
-  // console.log(token);
-  if (!token) {
-    logoutBtn.innerText = "Log In";
-  }
-  if (!token) {
-    createPostBtn.addEventListener("click", (e) => {
-      window.location.href = "http://127.0.0.1:5500/client/login.html";
-    });
-  }
-  // if (!token) {
-  //   applyBtn.addEventListener("click", (e) => {
-  //     console.log(e);
-  //     window.location.href = "http://127.0.0.1:5500/client/login.html";
-  //   });
-  // }
-};
-
-isLogin();
