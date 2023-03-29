d97e041cc733164cd4b7cba23998afa35004208e
brayson98 <brayson@live.com>
2023-03-29 14:50:50 +0100

    volunteer posts on user page

--- api/controllers/volunteerController.js
+++ api/controllers/volunteerController.js
@@ -9,6 +9,16 @@ async function index(req, res) {
         res.status(500).json({ error: err.message });
     }
 };
+async function getByUserId(req, res) {
+  try {
+    const queryArr = Object.keys(req.query);
+    const userId = req.query[queryArr[0]];
+    const volunteers = await volunteerModel.getOneById(userId, queryArr[0]);
+    res.status(200).json(volunteers);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+  }
   
 async function show(req, res) {
     try {
@@ -60,5 +70,6 @@ module.exports = {
     show,
     create,
     update,
-    destroy
+    destroy,
+    getByUserId
 };
