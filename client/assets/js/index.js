541bc1c5bf6736528c136968daf663ee1da14c84
MHaroonNasir <hrn-nsr@hotmail.com>
2023-03-29 09:20:23 +0100

    added button to posts

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -15,16 +15,35 @@ searchInput.addEventListener("input", (e) => {
   });
 });
 
+const volunteer = (e) => {
+  console.log("called", e.target.classList[1])
+  const cardId = e.target.classList[1];
+};
+
+const applyToVolunteer = () => {
+  const applyToPost = Array.from(document.getElementsByClassName("button-class"));
+  //console.log(applyToPost)
+  applyToPost.forEach(button => {
+    button.addEventListener("click", volunteer)
+  });
+};
+
 async function getAllPosts() {
 
   await fetch(`http://localhost:3000/posts`)
     .then((response) => response.json())
     .then((data) => {
+      //console.log(data)
       posts = data.map((post) => {
         const card = postCardTemplate.content.cloneNode(true).children[0];
         const title = card.querySelector("[data-title]");
         const content = card.querySelector("[data-content]");
         const category = card.querySelector("[data-category]");
+
+        const buttonClass = card.querySelector("[button-class]")
+        console.log("post", buttonClass)
+        buttonClass.classList.add(post.post_id);
+
         title.textContent = post.title;
         content.textContent = post.content;
         category.textContent = post.category;
@@ -37,5 +56,7 @@ async function getAllPosts() {
         };
       });
     });
+  
+  applyToVolunteer();
 }
 getAllPosts();
